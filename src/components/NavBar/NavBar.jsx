96202d7c12c588816eebe6669b1ee3e08129233d
TendyAlert <tedmanz2003@gmail.com>
2024-04-02 15:10:55 -0500

    nav bar tabs setup
    nav bar tabs are setup may change colors though

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 import { Nav } from 'react-bootstrap'
 import './NavBar.css'
 
@@ -13,15 +13,21 @@ export default function NavBar() {
   }
 
   return (
-    <Nav className='nav-bar' activeKey={'/'} onSelect={handleSelect}>
+    <Nav className='nav-bar' activeKey={activeKey} onSelect={handleSelect}>
       <div className='nav-bar-header'>
         <h2>React/Redux ToDo List</h2>
       </div>
-      <div className="nav-link-container">
-        <Router className="nav-links">
-          <Nav.Item><Nav.Link to="/" eventKey={"/"}>Todos</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link to="/about" eventKey={"/about"}>About</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link to="/contact" eventKey={"/contact"}>Contact</Nav.Link></Nav.Item>
+      <div className='nav-link-container'>
+        <Router>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/" eventKey={"/"}>Todos</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/about" eventKey={"/about"}>About</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/contact" eventKey={"/contact"}>Contact</Nav.Link>
+          </Nav.Item>
         </Router>
       </div>
     </Nav>
